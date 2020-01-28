3c20f78a8018bc600d11815dcaff124a6dea83c0
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-01-28 10:47:12 -1000

    hachwhich2 part 7

--- Hackwich 2/ViewController.swift
+++ Hackwich 2/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func pressmebuttonpressed(_ sender: Any) {
+        print (" hello world ")
+
+        
+    }
 
 }
 
